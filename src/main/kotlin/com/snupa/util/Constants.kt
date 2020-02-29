0252722ef8d92fc9c27a83a5aaf8ba07663607ec
BaekGeunYoung <dvmflstm@naver.com>
2020-03-01 01:48:46 +0900

    전역으로 사용할만한 상수를 모아두는 Constants 클래스 생성

---
+++ src/main/kotlin/com/snupa/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.snupa.util
+
+class Constants {
+    companion object {
+        val KAKAO_TOKEN_URI = "https://kauth.kakao.com/oauth/token"
+        val KAKAO_USER_INFO_URI = "https://kapi.kakao.com/v1/user/me"
+    }
+}
