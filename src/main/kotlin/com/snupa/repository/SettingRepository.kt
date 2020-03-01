bf6247fc527828dac5dcc1f465b45752a551a30c
BaekGeunYoung <dvmflstm@naver.com>
2020-03-01 18:27:05 +0900

    필요없는 파일 삭제

--- src/main/kotlin/com/snupa/repository/SettingRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.snupa.repository
-
-import com.snupa.domain.Setting
-import org.springframework.data.repository.CrudRepository
-
-interface SettingRepository : CrudRepository<Setting, String>
