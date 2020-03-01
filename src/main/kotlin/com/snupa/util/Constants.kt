6db11515dfe3929427b698780f8e67c555f6e806
BaekGeunYoung <dvmflstm@naver.com>
2020-03-01 18:17:26 +0900

    access token을 이용해 카카오 사용자 정보를 받아오는 함수 작성

--- src/main/kotlin/com/snupa/util/Constants.kt
+++ src/main/kotlin/com/snupa/util/Constants.kt
@@ -3,6 +3,6 @@ package com.snupa.util
 class Constants {
     companion object {
         val KAKAO_TOKEN_URI = "https://kauth.kakao.com/oauth/token"
-        val KAKAO_USER_INFO_URI = "https://kapi.kakao.com/v1/user/me"
+        val KAKAO_USER_INFO_URI = "https://kapi.kakao.com/v2/user/me"
     }
 }
--- src/main/kotlin/com/snupa/util/KakaoAPI.kt
+++ src/main/kotlin/com/snupa/util/KakaoAPI.kt
@@ -1,8 +1,12 @@
 package com.snupa.util
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.DeserializationConfig
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.snupa.exception.KakaoGetAccessTokenException
+import com.snupa.exception.KakaoGetUserInfoException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.*
@@ -12,14 +16,33 @@ import java.net.URL
 
 @Service
 class KakaoAPI(
-        @Value("\${spring.security.oauth.client.registration.kakao.client-id}")
+        @Value("\${spring.security.oauth.client.registration.kakao.client-id:4742178bb6bdac8c5fe5cff3aff900d2}")
         private val kakaoClientId: String,
-        @Value("\${spring.security.oauth.client.registration.kakao.redirect-uri}")
+        @Value("\${spring.security.oauth.client.registration.kakao.redirect-uri:http://localhost:8080/oauth}")
         private val kakaoRedirectUri: String
 ) {
+    private val jsonMapper = jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false)
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
     data class TokenResponse(
             var access_token: String,
             var refresh_token: String?
+    )
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    data class UserInfoResponse(
+            var id: Long,
+            var properties: UserProperties
+    )
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    class UserProperties(
+            var nickname: String
+    )
+
+    class KakaoUserInfoResult(
+            var userId: Long,
+            var nickname: String
     )
 
     fun getAccessToken(authorize_code: String): TokenResponse {
@@ -47,22 +70,54 @@ class KakaoAPI(
 
             //    요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
             val br = BufferedReader(InputStreamReader(conn.inputStream))
-            var line = ""
+            var line: String? = ""
             var result = ""
-            while (br.readLine().also { line = it } != null) {
+
+            line = br.readLine()
+            while (line != null) {
                 result += line
+                line = br.readLine()
             }
 
             br.close()
             bw.close()
 
-            val jsonMapper = jacksonObjectMapper()
-
             return jsonMapper.readValue(result)
         } catch (e: IOException) {
+            e.printStackTrace()
             throw KakaoGetAccessTokenException(e.message)
         }
     }
 
+    fun getUserInfo(accessToken: String): KakaoUserInfoResult {
+        val reqURL = Constants.KAKAO_USER_INFO_URI
+        try {
+            val url = URL(reqURL)
+            val conn = url.openConnection() as HttpURLConnection
+            conn.requestMethod = "GET"
 
+            //    요청에 필요한 Header에 포함될 내용
+            conn.setRequestProperty("Authorization", "Bearer $accessToken")
+            val responseCode = conn.responseCode
+
+            val br = BufferedReader(InputStreamReader(conn.inputStream))
+            var line: String? = ""
+            var result = ""
+
+            line = br.readLine()
+            while (line != null) {
+                result += line
+                line = br.readLine()
+            }
+
+            val response: UserInfoResponse = jsonMapper.readValue(result)
+
+            val userId = response.id
+            val nickname = response.properties.nickname
+
+            return KakaoUserInfoResult(userId, nickname)
+        } catch (e: IOException) {
+            throw KakaoGetUserInfoException(e.message)
+        }
+    }
 }
