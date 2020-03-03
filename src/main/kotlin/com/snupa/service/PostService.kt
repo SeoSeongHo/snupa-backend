f36c3808096bfce9d04ce47d7899b104c84a5ab7
BaekGeunYoung <dvmflstm@naver.com>
2020-03-04 00:56:43 +0900

    PostService 인터페이스 약간 수정

--- src/main/kotlin/com/snupa/service/PostService.kt
+++ src/main/kotlin/com/snupa/service/PostService.kt
@@ -1,11 +1,15 @@
 package com.snupa.service
 
 import com.snupa.domain.Post
+import com.snupa.dto.oauth.AuthPrincipal
+import com.snupa.dto.post.PostReq
+import com.snupa.dto.post.SearchType
+import org.springframework.web.multipart.MultipartFile
 
 interface PostService {
-    fun getPosts(recent: Boolean, popular: Boolean): List<Post>
+    fun getPosts(recent: Boolean, popular: Boolean, type: SearchType): List<Post>
     fun getPost(postId: Long): Post
-    fun createPost(): Post
-    fun updatePost(): Post
-    fun deletePost(postId: Long): Unit
+    fun createPost(files: List<MultipartFile>?, postReq: PostReq, authPrincipal: AuthPrincipal): Post
+    fun updatePost(files: List<MultipartFile>?, postId: Long, postReq: PostReq, authPrincipal: AuthPrincipal): Post
+    fun deletePost(postId: Long, authPrincipal: AuthPrincipal): Unit
 }
