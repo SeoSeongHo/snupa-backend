6bf671da6180239dd4d637f0d490c42e6a57e62b
BaekGeunYoung <dvmflstm@naver.com>
2020-03-04 00:56:00 +0900

    PostController 객체에 필요 api 작성 완료

--- src/main/kotlin/com/snupa/controller/PostController.kt
+++ src/main/kotlin/com/snupa/controller/PostController.kt
@@ -1,12 +1,17 @@
 package com.snupa.controller
 
 import com.snupa.domain.Post
+import com.snupa.dto.oauth.AuthPrincipal
+import com.snupa.dto.post.PostReq
 import com.snupa.dto.post.SearchType
 import com.snupa.service.PostService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -27,5 +32,32 @@ class PostController(
     fun getPost(@PathVariable("id") postId: Long): ResponseEntity<Post> {
         val post = postService.getPost(postId)
         return ResponseEntity(post, HttpStatus.OK)
+    }
+
+    @PostMapping("/")
+    fun createPost(@RequestParam("files") files: List<MultipartFile>?,
+                   @RequestBody @Valid postReq: PostReq,
+                   @AuthenticationPrincipal authPrincipal: AuthPrincipal
+    ): ResponseEntity<Post> {
+        val post = postService.createPost(files, postReq, authPrincipal)
+        return ResponseEntity(post, HttpStatus.CREATED)
+    }
+
+    @PutMapping("/{id}")
+    fun updatePost(@RequestParam("files") files: List<MultipartFile>?,
+                   @PathVariable("id") postId: Long,
+                   @RequestBody @Valid postReq: PostReq,
+                   @AuthenticationPrincipal authPrincipal: AuthPrincipal
+    ): ResponseEntity<Post> {
+        val post = postService.updatePost(files, postId, postReq, authPrincipal)
+        return ResponseEntity(post, HttpStatus.OK)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePost(@PathVariable("id") postId: Long,
+                   @AuthenticationPrincipal authPrincipal: AuthPrincipal
+    ): ResponseEntity<Unit> {
+        postService.deletePost(postId, authPrincipal)
+        return ResponseEntity(HttpStatus.NO_CONTENT)
     }
 }
