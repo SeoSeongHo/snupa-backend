7e55e88bec4923c9400af78a3c4215c256685480
Seo Seong Ho <swa04246@naver.com>
2020-03-04 01:20:12 +0900

    create user service layer

---
+++ src/main/kotlin/com/snupa/service/user/UserServiceImpl.kt
@@ -0,0 +1,28 @@
+package com.snupa.service.user
+
+import com.snupa.dto.user.UserReq
+import com.snupa.dto.user.UserRes
+import com.snupa.exception.UserIdNotFoundException
+import com.snupa.mapper.UserMapper
+import com.snupa.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class UserServiceImpl(
+        @Autowired private val userRepository : UserRepository
+) : UserService {
+    override fun getUser(userId: Long): UserRes {
+        val user = userRepository.findById(userId)
+        if(user.isPresent)
+            throw UserIdNotFoundException("can not find user : ${userId}")
+
+        return UserMapper.toDto(user.get())
+    }
+
+    override fun updateUser(userReq : UserReq): UserRes {
+        val user = userRepository.save(UserMapper.toEntity(userReq))
+
+        return UserMapper.toDto(user)
+    }
+}
