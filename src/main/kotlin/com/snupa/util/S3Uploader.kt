ee04c75ce76a0b8b35d1579ed121d700debe948d
BaekGeunYoung <dvmflstm@naver.com>
2020-03-07 23:53:19 +0900

    s3 upload를 위한 유틸 클래스 작성

--- src/main/kotlin/com/snupa/util/S3Uploader.kt
+++ src/main/kotlin/com/snupa/util/S3Uploader.kt
@@ -1,2 +1,57 @@
 package com.snupa.util
 
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.PutObjectRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
+
+@Component
+class S3Uploader(
+        @Autowired
+        private val amazonS3Client: AmazonS3Client,
+        @Value("\${cloud.aws.s3.bucket}")
+        private val bucket: String,
+        @Value("\${cloud.aws.s3.dir}")
+        private val dirName: String
+) {
+    @Throws(IOException::class)
+    fun upload(multipartFile: MultipartFile): String? {
+        val uploadFile: File = convert(multipartFile)
+                .orElseThrow { IllegalArgumentException("Converting MultipartFile to File failed") }
+        return upload(uploadFile)
+    }
+
+    private fun upload(uploadFile: File): String? {
+        val fileName = dirName + "/" + uploadFile.name
+        val uploadImageUrl = putS3(uploadFile, fileName)
+        removeNewFile(uploadFile)
+        return uploadImageUrl
+    }
+
+    private fun putS3(uploadFile: File?, fileName: String?): String? {
+        amazonS3Client.putObject(PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead))
+        return amazonS3Client.getUrl(bucket, fileName).toString()
+    }
+
+    private fun removeNewFile(targetFile: File) {
+        targetFile.delete()
+    }
+
+    @Throws(IOException::class)
+    private fun convert(file: MultipartFile): Optional<File> {
+        val convertFile = File(file.originalFilename!!)
+        if (convertFile.createNewFile()) {
+            FileOutputStream(convertFile).use { it.write(file.bytes) }
+            return Optional.of(convertFile)
+        }
+        return Optional.empty()
+    }
+}
