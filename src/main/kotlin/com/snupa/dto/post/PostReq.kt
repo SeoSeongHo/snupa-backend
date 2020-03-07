fbb3177e47f726540bba4a0daaf5efe2219c9dd2
BaekGeunYoung <dvmflstm@naver.com>
2020-03-08 01:05:58 +0900

    PostReq 객체에 빠뜨린 category 필드 추가

--- src/main/kotlin/com/snupa/dto/post/PostReq.kt
+++ src/main/kotlin/com/snupa/dto/post/PostReq.kt
@@ -1,8 +1,11 @@
 package com.snupa.dto.post
 
+import com.snupa.util.EnumCollection
+
 data class PostReq(
         var title: String,
         var content: String,
         var placeId: Long,
+        var category: EnumCollection.Category,
         var date: EventDate
 )
