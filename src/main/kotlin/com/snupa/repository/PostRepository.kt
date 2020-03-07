06ff02243f07e5be98192f98e9a6e2a2211d7af2
BaekGeunYoung <dvmflstm@naver.com>
2020-03-08 01:04:51 +0900

    postRepository에 카테고리별 조회 함수 추가

--- src/main/kotlin/com/snupa/repository/PostRepository.kt
+++ src/main/kotlin/com/snupa/repository/PostRepository.kt
@@ -1,6 +1,10 @@
 package com.snupa.repository
 
+import com.snupa.domain.Like
 import com.snupa.domain.Post
+import com.snupa.util.EnumCollection
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository : JpaRepository<Post, Long>
+interface PostRepository : JpaRepository<Post, Long> {
+    fun findByCategory(category: EnumCollection.Category): MutableList<Post>
+}
