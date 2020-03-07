0c5e06294ec4181b39fa7ed4c7040dedbabde9ed
BaekGeunYoung <dvmflstm@naver.com>
2020-03-07 23:53:46 +0900

    conflict resolving을 위해 작성중이던 PostServiceImpl 커밋

--- src/main/kotlin/com/snupa/service/PostServiceImpl.kt
+++ src/main/kotlin/com/snupa/service/PostServiceImpl.kt
@@ -6,13 +6,15 @@ import com.snupa.dto.post.PostReq
 import com.snupa.dto.post.SearchType
 import com.snupa.exception.PostNotFoundException
 import com.snupa.repository.PostRepository
+import com.snupa.util.S3Uploader
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
 class PostServiceImpl(
-        @Autowired private val postRepository: PostRepository
+        @Autowired private val postRepository: PostRepository,
+        @Autowired private val s3Uploader: S3Uploader
 ): PostService {
     override fun getPosts(recent: Boolean, popular: Boolean, type: SearchType): List<Post> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -26,7 +28,8 @@ class PostServiceImpl(
     }
 
     override fun createPost(files: List<MultipartFile>?, postReq: PostReq, authPrincipal: AuthPrincipal): Post {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val imageUrls = files?.map { s3Uploader.upload(it) }
+
     }
 
     override fun updatePost(files: List<MultipartFile>?, postId: Long, postReq: PostReq, authPrincipal: AuthPrincipal): Post {
