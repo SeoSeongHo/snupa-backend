11f443bfe8ff6fd9eb9c33df4924ef6e6891977c
BaekGeunYoung <dvmflstm@naver.com>
2020-03-08 01:06:54 +0900

    Post 도메인 객체에 필요한 여러 연산 추가

--- src/main/kotlin/com/snupa/domain/Post.kt
+++ src/main/kotlin/com/snupa/domain/Post.kt
@@ -1,6 +1,8 @@
 package com.snupa.domain
 
+import com.snupa.dto.post.PostReq
 import com.snupa.util.EnumCollection
+import org.springframework.web.multipart.MultipartFile
 import java.time.LocalDateTime
 import javax.persistence.*
 
@@ -36,4 +38,39 @@ data class Post (
 
         @OneToMany(fetch=FetchType.LAZY, mappedBy = "post")
         var likes : MutableList<Like>
-) : EntityAuditing()
+) : EntityAuditing() {
+        companion object {
+                fun of(
+                        postReq: PostReq,
+                        user: User,
+                        poster: String?
+                ): Post {
+                        return Post(
+                                title = postReq.title,
+                                content = postReq.content,
+                                startedAt = postReq.date.from,
+                                endedAt = postReq.date.to,
+                                category = postReq.category,
+                                poster = poster,
+                                user = user,
+                                comments = mutableListOf(),
+                                likes = mutableListOf()
+                        )
+                }
+        }
+
+        fun update(postReq: PostReq, poster: String?): Post {
+                this.title = postReq.title
+                this.content = postReq.content
+                this.category = postReq.category
+                this.endedAt = postReq.date.to
+                this.startedAt = postReq.date.from
+                this.poster = poster
+
+                return this
+        }
+
+        fun isOwnedBy(userId: Long): Boolean {
+                return this.user.id!! == userId
+        }
+}
